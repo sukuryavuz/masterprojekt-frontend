073176b272b4fc24fca7248de2a5cc144b010ce5
sukuryavuz <sukur.yavuz@hotmail.com>
2023-01-16 16:47:53 +0100

    register return error if user can not register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { Userr } from '../shared/user';
@@ -13,9 +13,11 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  imports: [MatFormFieldModule, FormsModule, MatDialogModule, MatInputModule, MatButtonModule]
+  imports: [MatFormFieldModule, FormsModule, MatDialogModule, MatInputModule, MatButtonModule, ReactiveFormsModule]
 })
 export class RegisterComponent {
+  public form:FormGroup;
+
   readonly model = {
     firstname: this.data.firstname,
     lastname: this.data.lastname,
@@ -27,19 +29,31 @@ export class RegisterComponent {
     @Inject(MAT_DIALOG_DATA) private readonly data: Userr,
     public dialogRef: MatDialogRef<RegisterComponent, boolean>,
     private loginService: LoginService,
-    private snackBar: MatSnackBar
-  ) {}
+    private snackBar: MatSnackBar,
+    private fb:FormBuilder
+  ) {
+    this.form = fb.group({
+      firstname:[null, Validators.required],
+      lastname:[null, Validators.required],
+      username:[null, Validators.required],
+      password:[null, Validators.required]
+    })
+  }
 
   cancel(): void {
     this.dialogRef.close();
   }
 
-  registrieren(form: NgForm): void {
+  registrieren(): void {
+    if(this.form.valid) {
     this.loginService
       .register(this.model.firstname, this.model.lastname, this.model.username, this.model.password)
       .subscribe(() => {
         this.snackBar.open('Sie haben sich erfolgreich registriert. Nun können Sie sich einloggen', 'X');
         this.dialogRef.close(true);
       });
+    } else {
+      this.snackBar.open('Sie müssen alle Pflichtfelder ausfüllen, um fortfahren zu können.', 'X');
+    }
   }
 }
