6cd6c9eb8b2d287e67d28e41b6a6bb785c74b292
sukuryavuz <sukur.yavuz@hotmail.com>
2023-01-17 14:25:22 +0100

    handle Error in login

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -7,33 +7,43 @@ import { LoginService } from 'src/app/service/login/login.service';
 import { User } from 'src/app/shared/user';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { BannerService } from 'src/app/banner/banner.service';
 import { MatDialog } from '@angular/material/dialog';
 import { RegisterComponent } from 'src/app/register/register.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   standalone: true,
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule, RegisterComponent]
+  imports: [MatCardModule, MatFormFieldModule, MatButtonModule, MatInputModule, RegisterComponent, ReactiveFormsModule]
 })
 export class LoginComponent {
+  public form:FormGroup;
+
   firstname: string;
   lastname: string;
   username: string;
   password: string;
 
   constructor(
+    fb: FormBuilder,
     private loginService: LoginService,
     private router: Router,
     private snackBar: MatSnackBar,
-    private bannerService: BannerService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
   ) {
+    this.form = fb.group({
+      username:[null, Validators.required],
+      password: [null, Validators.required]
+    })
     this.loginService.logout();
   }
 
-  login(username:string, password:string) {
+  login(username:string, password:string): boolean {
+    if(this.form.controls['username'].invalid || this.form.controls['password'].invalid) {
+      this.snackBar.open('Sie müssen einen Usernamen und ein Passwort eingeben, um sich anzumelden.', 'X');
+      return false;
+    }
     this.loginService
       .login(username, password)
       .subscribe(data => {
@@ -45,6 +55,7 @@ export class LoginComponent {
         this.loginService.setIsLoggedIn(true);
         this.router.navigate(['/available-products']);
       })
+      return true;
   }
 
   register(): void {
